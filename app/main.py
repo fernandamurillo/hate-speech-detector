aa967f9dc07a4a633697f8c83f1186a5920056d8
Fernanda <fernanda.murillo496@gmail.com>
2021-07-23 20:19:15 +0000

    ready for production deployment

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
-# Run by typing python3 main.py
-
-## **IMPORTANT:** only collaborators on the project where you run
-## this can access this web server!
-
-"""
-    Bonus points if you want to have internship at AI Camp
-    1. How can we save what user built? And if we can save them, like allow them to publish, can we load the saved results back on the home page? 
-    2. Can you add a button for each generated item at the frontend to just allow that item to be added to the story that the user is building? 
-    3. What other features you'd like to develop to help AI write better with a user? 
-    4. How to speed up the model run? Quantize the model? Using a GPU to run the model? 
-"""
-
 # import basics
 import os
+import pickle
+from text_cleaner import clean_text
+from flask_cors import cross_origin
+
+# preprocessing imports
+import numpy as np
+import re
+from sklearn.feature_extraction.text import CountVectorizer
 
 # import stuff for our web server
 from flask import Flask, flash, request, redirect, url_for, render_template
@@ -20,71 +15,70 @@ from flask import send_from_directory
 from flask import jsonify
 from utils import get_base_url, allowed_file, and_syntax
 
-# import stuff for our models
-import torch
-from aitextgen import aitextgen
-
 '''
 Coding center code - comment out the following 4 lines of code when ready for production
 '''
 # load up the model into memory
 # you will need to have all your trained model in the app/ directory.
-ai = aitextgen(to_gpu=False, model=r"EleutherAI/gpt-neo-125M")
+model = pickle.load(open("log_reg_model.sav", 'rb'))
+cv = pickle.load(open("countvec.pkl",'rb'))
+
+# preprocessing
+NON_ALPHANUM = re.compile(r'[\W]')
+NON_ASCII = re.compile(r'[^a-z0-1\s]')
+def normalize_texts(texts):
+    normalized_texts = []
+    for text in texts:
+        lower = text.lower()
+        no_punctuation = NON_ALPHANUM.sub(r' ', lower)
+        no_non_ascii = NON_ASCII.sub(r'', no_punctuation)
+        normalized_texts.append(no_non_ascii)
+    return normalized_texts
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
-port = 12345
-base_url = get_base_url(port)
-app = Flask(__name__, static_url_path=base_url+'static')
+# port = 12335
+# base_url = get_base_url(port)
+# app = Flask(__name__, static_url_path=base_url+'static')
 
-'''
-Deployment code - uncomment the following line of code when ready for production
-'''
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route('/')
-@app.route(base_url)
+@app.route('/')
+# @app.route(base_url)
 def home():
-    return render_template('writer_home.html', generated=None)
+    return render_template('home.html', generated=None)
 
-#@app.route('/', methods=['POST'])
-@app.route(base_url, methods=['POST'])
-def home_post():
-    return redirect(url_for('results'))
-
-#@app.route('/results')
-@app.route(base_url + '/results')
+@app.route('/results', methods=["GET","POST"])
+@cross_origin()
 def results():
-    return render_template('Write-your-story-with-AI.html', generated=None)
+    if request.method=="POST":
+        
+        normalized_review = []
+        review =(request.form["Review"])
+        
+        review = [review]
+        cleaned_texts = normalize_texts(review)
+        cleaned_texts = cv.transform(cleaned_texts)                
 
-#@app.route('/generate_text', methods=["POST"])
-@app.route(base_url + '/generate_text', methods=["POST"])
-def generate_text():
-    """
-    view function that will return json response for generated text. 
-    """
+        prediction=model.predict(cleaned_texts)
+        
+        output=""
+        if prediction[0]==0:
+            output="Negative"
 
-    prompt = request.form['prompt']
-    if prompt is not None:
-        generated = ai.generate(
-            n=3,
-            batch_size=3,
-            prompt=str(prompt),
-            max_length=50,
-            temperature=0.9,
-            return_as_list=True
-        )
+        else:
+            output="Positive"
 
-    data = {'generated_ls': generated}
+        return render_template('home.html',prediction_text=f'This is a {output} Review')
 
-    return jsonify(data)
+    return render_template("home.html")
 
 if __name__ == "__main__":
     '''
     coding center code
     '''
     # IMPORTANT: change the cocalcx.ai-camp.org to the site where you are editing this file.
-    website_url = 'cocalcx.ai-camp.org'
+    website_url = 'cocalc8.ai-camp.org'
     print(f"Try to open\n\n    https://{website_url}" + base_url + '\n\n')
 
     app.run(host = '0.0.0.0', port=port, debug=True)
